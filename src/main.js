9bda7c78962a081d7b300aee97003702f8b21395
MuhaddiMu <muhaddisshah@gmail.com>
2019-07-04 20:21:29 +0500

    Vuetify Added

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 import Vue from "vue";
+import './plugins/vuetify'
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+
+Vue.use(Vuetify)
 
 Vue.config.productionTip = false;
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,6 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="about">
+    <h1 class="display-3 grey darken-1 text-uppercase font-weight-regular black--text center text-xs-center">Home</h1>
+    <p class="body-2 text-xs-center grey grey lighten-3 text--grey">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas saepe rerum atque architecto, quam natus sed id reprehenderit libero accusantium dolorum commodi itaque quo laboriosam eum tempore, animi quisquam hic.</p>
   </div>
 </template>
-
-<script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
-export default {
-  name: "home",
-  components: {
-    HelloWorld
-  }
-};
-</script>
