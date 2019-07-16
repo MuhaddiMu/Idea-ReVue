f47abf745338191770178a0012094f8dafec9897
MuhaddiMu <muhaddisshah@gmail.com>
2019-07-16 15:50:23 +0500

    Validation Rules Added

--- src/views/Account.vue
+++ src/views/Account.vue
@@ -12,10 +12,10 @@
                 <v-text-field prepend-icon="person" label="Full Name" required></v-text-field>
               </v-flex>
 			  <v-flex xs12>
-                <v-text-field prepend-icon="email" label="Email" required></v-text-field>
+                <v-text-field prepend-icon="email" label="Email" required :rules="[rules.EmailRequired, rules.EmailValid]"></v-text-field>
               </v-flex>
               <v-flex xs12>
-                <v-text-field prepend-icon="lock" label="Password" type="password" required></v-text-field>
+                <v-text-field prepend-icon="lock" label="Password" required v-model="RegPass" :append-icon="Pass ? 'visibility' : 'visibility_off'" :rules="[rules.PassRequired, rules.PassMin]" :type="Pass ? 'text' : 'password'" hint="At least 8 characters" counter @click:append="Pass = !Pass" ></v-text-field>
               </v-flex>
             </v-layout>
           </v-container>
@@ -34,13 +34,22 @@
 export default {
 	data(){
 		return{
-			dialog: false,
+      dialog: false,
+      Pass: false,
+      RegPass: '',
+      rules: {
+				PassRequired: value => !!value || 'Required.',
+				PassMin: v => v.length >= 8 || 'Min 8 characters',
+				EmailRequired: v => !!v || 'E-mail is required',
+				EmailValid: v => /.+@.+/.test(v) || 'E-mail must be valid'
+			},
 		}
   },
   
   methods: {
     ShowModal(){
       this.dialog = true;
+      
     }
   }
 }
