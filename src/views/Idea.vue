b39f3ec63ce478733b90ce6a42be50857806f00c
MuhaddiMu <muhaddisshah@gmail.com>
2019-07-22 15:19:31 +0500

    Idea Modal Created

--- src/views/Idea.vue
+++ src/views/Idea.vue
@@ -10,9 +10,23 @@
 					<v-card-title>
 						<span class="headline">Wohoo! You Got An Idea...</span>
 					</v-card-title>
+
+					<v-card-text>
+						<v-container grid-list-md>
+							<v-layout wrap>
+								<v-flex xs12>
+									<v-text-field prepend-inner-icon="edit" :rules="[Rules.Title]" label="Title" box></v-text-field>
+								</v-flex>
+								<v-flex xs12>
+									<v-text-field prepend-inner-icon="format_align_left"  label="Description" append-icon="star" box></v-text-field>
+								</v-flex>
+							</v-layout>
+						</v-container>
+					</v-card-text>
 					<v-card-actions>
+					<v-spacer></v-spacer>
 						<v-btn flat @click="dialog = false">Close</v-btn>
-						<v-btn :loading="Loading" flat >Save</v-btn>
+						<v-btn class="primary" :loading="Loading" flat >Save</v-btn>
 					</v-card-actions>
 				</v-form>
 			</v-card>
@@ -30,6 +44,9 @@ export default {
 			Snackbar: false,
 			SnackbarMsg: '',
 			Loading: false,
+			Rules: {
+				Title: value => !!value || 'Required.',
+			}
 		}
 	},
 
