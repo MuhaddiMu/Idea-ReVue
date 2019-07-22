ed8f2331aeae17e32db02ff0d8060c76877cf537
MuhaddiMu <muhaddisshah@gmail.com>
2019-07-22 13:09:00 +0500

    Idea Modal Created

---
+++ src/views/Idea.vue
@@ -0,0 +1,47 @@
+<template>
+	<v-layout row justify-center>
+		<v-snackbar v-model="Snackbar" top>{{SnackbarMsg}}
+			<v-btn color="pink" flat @click="Snackbar = false"> Close</v-btn>
+		</v-snackbar>
+		
+		<v-dialog v-model="dialog" persistent max-width="600px">
+			<v-card>
+				<v-form ref="Form">
+					<v-card-title>
+						<span class="headline">Wohoo! You Got An Idea...</span>
+					</v-card-title>
+					<v-card-actions>
+						<v-btn flat @click="dialog = false">Close</v-btn>
+						<v-btn :loading="Loading" flat >Save</v-btn>
+					</v-card-actions>
+				</v-form>
+			</v-card>
+		</v-dialog>
+	</v-layout>
+</template>
+
+<script>
+import firebase from '../firebase'
+import store from '../store';
+export default {
+	data() {
+		return {
+			dialog: false,
+			Snackbar: false,
+			SnackbarMsg: '',
+			Loading: false,
+		}
+	},
+
+	methods: {
+		ShowModal() {
+			this.dialog = true;
+		},
+
+	},
+}
+</script>
+
+<style>
+
+</style>
--- src/views/Navbar.vue
+++ src/views/Navbar.vue
@@ -35,11 +35,22 @@
            Update Account
           </v-list-tile-content>
         </v-list-tile>
+        <v-list-tile @click="ShowIdeaModal" v-ripple>
+          <v-list-tile-action>
+            <svg colo style="width:24px;height:24px" viewBox="0 0 24 24">
+                <path fill="#00000" fill-opacity="0.5" d="M12,6A6,6 0 0,1 18,12C18,14.22 16.79,16.16 15,17.2V19A1,1 0 0,1 14,20H10A1,1 0 0,1 9,19V17.2C7.21,16.16 6,14.22 6,12A6,6 0 0,1 12,6M14,21V22A1,1 0 0,1 13,23H11A1,1 0 0,1 10,22V21H14M20,11H23V13H20V11M1,11H4V13H1V11M13,1V4H11V1H13M4.92,3.5L7.05,5.64L5.63,7.05L3.5,4.93L4.92,3.5M16.95,5.63L19.07,3.5L20.5,4.93L18.37,7.05L16.95,5.63Z" />
+            </svg>
+          </v-list-tile-action>
+          <v-list-tile-content class="grey--text">
+           Got an Idea?
+          </v-list-tile-content>
+        </v-list-tile>
       </v-list>
   </v-navigation-drawer>
   <v-snackbar v-model="LogSuccess" top>Success! Welcome to Dashboard<v-btn color="pink" flat @click="LogSuccess = false" > Close</v-btn></v-snackbar>
 
   <Account ref="Account"></Account>
+  <Idea ref="Idea"></Idea>
 
 
 </nav>
@@ -49,10 +60,12 @@
 import firebase from '../firebase';
 import store from '../store';
 import Account from './Account';
+import Idea from './Idea';
 export default {
 
   components: {
     Account,
+    Idea,
   },
 
 	data(){
@@ -71,6 +84,10 @@ export default {
     },
     ShowModal(){
       this.$refs.Account.ShowModal();
+    },
+    
+    ShowIdeaModal(){
+      this.$refs.Idea.ShowModal();
     },
 
     LoveUpdate(){
