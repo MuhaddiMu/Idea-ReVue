17e759dd007f4374eb4a4551752de64ce1cac4a4
MuhaddiMu <Muhaddisshah@gmail.com>
2019-09-06 01:08:05 +0500

    Packages Updated

--- src/views/MyIdeas.vue
+++ src/views/MyIdeas.vue
@@ -6,7 +6,7 @@
 			<v-flex class="ml-2" xs6 md3>
 				<v-card hover max-width="320" class="mx-auto">
 					<v-card-title class="title">My Card Title</v-card-title>
-					<v-card-text>Card Description Goes Here... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ...</v-card-text>
+					<v-card-text>Card Description Goes Here...Card Description Goes Here...Card Description Goes Here...Card Description Goes Here...</v-card-text>
 					<v-card-actions class="caption grey--text text--darken-3">
 						Added on: 4th September 2019<v-spacer></v-spacer>
             		<v-btn icon><v-icon>more_vert</v-icon></v-btn>
@@ -16,13 +16,30 @@
 			<v-flex class="ml-2" xs6 md3>
 				<v-card hover max-width="320" class="mx-auto">
 					<v-card-title class="title">My Card Title</v-card-title>
-					<v-card-text>Card Description Goes Here... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ...</v-card-text>
+					<v-card-text>Card Description Goes Here...Card Description Goes Here...Card Description Goes Here...Card Description Goes Here...</v-card-text>
 					<v-card-actions class="caption grey--text text--darken-3">
 						Added on: 4th September 2019<v-spacer></v-spacer>
             		<v-btn icon><v-icon>more_vert</v-icon></v-btn>
 					</v-card-actions>
 				</v-card>
 			</v-flex>
+
+				<v-menu offset-y>
+			<template v-slot:activator="{ on }">
+				<v-btn
+				color="primary"
+				dark
+				v-on="on"
+				>
+				Dropdown
+				</v-btn>
+			</template>
+			<v-list>
+				<v-list-item>
+				<v-list-item-title>Muhaddis</v-list-item-title>
+				</v-list-item>
+			</v-list>
+			</v-menu>
 
 		</v-layout>
 	</v-container>
