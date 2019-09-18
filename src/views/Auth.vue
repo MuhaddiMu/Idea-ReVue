00f8a591d47ab05321650533bc5af91a6bbdc79b
MuhaddiMu <Muhaddisshah@gmail.com>
2019-09-18 23:18:55 +0500

    Content Placeholder Added. -VueContentLoading

--- src/views/Auth.vue
+++ src/views/Auth.vue
@@ -1,61 +1,103 @@
 <template>
-	<v-container>
-		<h1 class="text-center display-3 blue--text Title">Idea Re-Vue</h1>
-		<v-snackbar v-model="Snackbar" top>{{SnackbarMsg}}<v-btn color="pink" text @click="Snackbar = false" > Close</v-btn></v-snackbar>
-		<v-layout align-center justify-center row wrap>
-			<v-flex xs12 md6>
-				<v-card v-if="Login" justify-center color="grey lighten-4">
-					<v-form ref="form">
-						<vue-headful title="Account Login | Idea Re-Vue"/>
+  <v-container>
+    <h1 class="text-center display-3 blue--text Title">Idea Re-Vue</h1>
+    <v-snackbar v-model="Snackbar" top>
+      {{SnackbarMsg}}
+      <v-btn color="pink" text @click="Snackbar = false">Close</v-btn>
+    </v-snackbar>
+    <v-layout align-center justify-center row wrap>
+      <v-flex xs12 md6>
+        <v-card v-if="Login" justify-center color="grey lighten-4">
+          <v-form ref="form">
+            <vue-headful title="Account Login | Idea Re-Vue" />
 
-						<v-card-text class="headline text-center px-0">Account Login</v-card-text>
+            <v-card-text class="headline text-center px-0">Account Login</v-card-text>
 
-						<v-flex align-self-center xs6 md8 offset-sm2>
-							<v-text-field v-model="LoginEmail" key="LoginEmail" label="Email" prepend-icon="email" :rules="[rules.EmailRequired, rules.EmailValid]"></v-text-field>
-						</v-flex>
+            <v-flex align-self-center xs6 md8 offset-sm2>
+              <v-text-field
+                v-model="LoginEmail"
+                key="LoginEmail"
+                label="Email"
+                prepend-icon="email"
+                :rules="[rules.EmailRequired, rules.EmailValid]"
+              ></v-text-field>
+            </v-flex>
 
-						<v-flex align-self-center xs6 md8 offset-sm2>
-							<v-text-field @keyup.enter="LogIn()" v-model="LoginPassword" key="LoginPassword" prepend-icon="lock" :append-icon="Pass ? 'visibility' : 'visibility_off'" :rules="[rules.PassRequired, rules.PassMin]" :type="Pass ? 'text' : 'password'" label="Password" hint="At least 8 characters" counter @click:append="Pass = !Pass" ></v-text-field>
-						</v-flex>
+            <v-flex align-self-center xs6 md8 offset-sm2>
+              <v-text-field
+                @keyup.enter="LogIn()"
+                v-model="LoginPassword"
+                key="LoginPassword"
+                prepend-icon="lock"
+                :append-icon="Pass ? 'visibility' : 'visibility_off'"
+                :rules="[rules.PassRequired, rules.PassMin]"
+                :type="Pass ? 'text' : 'password'"
+                label="Password"
+                hint="At least 8 characters"
+                counter
+                @click:append="Pass = !Pass"
+              ></v-text-field>
+            </v-flex>
 
+            <v-flex align-self-center xs6 md8 offset-sm4>
+              <v-btn @click="LogIn" text :loading="Loading">Log In</v-btn>
+              <v-btn @click="Login = !Login" text>Register</v-btn>
+            </v-flex>
+          </v-form>
+        </v-card>
 
-						<v-flex align-self-center xs6 md8 offset-sm4>
-							<v-btn @click="LogIn" text :loading="Loading">Log In</v-btn>
-							<v-btn @click="Login = !Login" text>Register</v-btn>
-						</v-flex>
-					</v-form>
-				</v-card>
+        <v-card v-else justify-center color="grey lighten-4">
+          <v-form ref="form">
+            <vue-headful title="Account Registeration | Idea Re-Vue" />
 
+            <v-card-text class="headline text-center px-0">Account Registeration</v-card-text>
 
-				<v-card v-else justify-center color="grey lighten-4">
-					<v-form ref="form">
-						<vue-headful title="Account Registeration | Idea Re-Vue"/>
+            <v-flex align-self-center xs6 md8 offset-sm2>
+              <v-text-field
+                v-model="RegName"
+                key="RegName"
+                label="Full Name"
+                prepend-icon="person"
+                :rules="[rules.NameReq]"
+              ></v-text-field>
+            </v-flex>
 
-						<v-card-text class="headline text-center px-0">Account Registeration</v-card-text>
+            <v-flex align-self-center xs6 md8 offset-sm2>
+              <v-text-field
+                v-model="RegEmail"
+                key="RegEmail"
+                label="Email"
+                prepend-icon="email"
+                :rules="[rules.EmailRequired, rules.EmailValid]"
+              ></v-text-field>
+            </v-flex>
 
-						<v-flex align-self-center xs6 md8 offset-sm2>
-							<v-text-field v-model="RegName" key="RegName" label="Full Name" prepend-icon="person" :rules="[rules.NameReq]"></v-text-field>
-						</v-flex>
+            <v-flex align-self-center xs6 md8 offset-sm2>
+              <v-text-field
+                @keyup.enter="Register()"
+                key="RegPass"
+                prepend-icon="lock"
+                v-model="RegPass"
+                :append-icon="Pass ? 'visibility' : 'visibility_off'"
+                :rules="[rules.PassRequired, rules.PassMin]"
+                :type="Pass ? 'text' : 'password'"
+                label="Password"
+                hint="At least 8 characters"
+                counter
+                @click:append="Pass = !Pass"
+              ></v-text-field>
+            </v-flex>
 
-						<v-flex align-self-center xs6 md8 offset-sm2>
-							<v-text-field v-model="RegEmail" key="RegEmail" label="Email" prepend-icon="email" :rules="[rules.EmailRequired, rules.EmailValid]"></v-text-field>
-						</v-flex>
+            <v-flex align-self-center xs6 md8 offset-sm4>
+              <v-btn @click="Login = !Login" text>Log In</v-btn>
 
-						<v-flex align-self-center xs6 md8 offset-sm2>
-							<v-text-field @keyup.enter="Register()" key="RegPass" prepend-icon="lock" v-model="RegPass" :append-icon="Pass ? 'visibility' : 'visibility_off'" :rules="[rules.PassRequired, rules.PassMin]" :type="Pass ? 'text' : 'password'" label="Password" hint="At least 8 characters" counter @click:append="Pass = !Pass" ></v-text-field>
-						</v-flex>
-
-
-						<v-flex align-self-center xs6 md8 offset-sm4>
-							<v-btn @click="Login = !Login" text>Log In</v-btn>
-
-							<v-btn @click="Register" text :loading="Loading">Register</v-btn>
-						</v-flex>
-					</v-form>
-				</v-card>
-			</v-flex>
-		</v-layout>
-	</v-container>
+              <v-btn @click="Register" text :loading="Loading">Register</v-btn>
+            </v-flex>
+          </v-form>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
@@ -63,77 +105,77 @@ import firebase from '../firebase';
 import router from '../router'
 
 export default {
-	data(){
-		return {
-			Snackbar: false,
-			SnackbarMsg: '',
-			Loading: false,
-			LoginEmail:	'',
-			LoginPassword:	'',
-			RegName:	'',
-			RegEmail:	'',
-			RegPass:	'',
-			Pass: false,
-			rules: {
-				PassRequired: value => !!value || 'Required.',
-				PassMin: v => v.length >= 8 || 'Min 8 characters',
-				EmailRequired: v => !!v || 'E-mail is required',
-				EmailValid: v => /.+@.+/.test(v) || 'E-mail must be valid',
-				NameReq: value => !!value || 'Required.',
-			},
-			Login: true,
-		}
-	},
+  data() {
+    return {
+      Snackbar: false,
+      SnackbarMsg: '',
+      Loading: false,
+      LoginEmail: '',
+      LoginPassword: '',
+      RegName: '',
+      RegEmail: '',
+      RegPass: '',
+      Pass: false,
+      rules: {
+        PassRequired: value => !!value || 'Required.',
+        PassMin: v => v.length >= 8 || 'Min 8 characters',
+        EmailRequired: v => !!v || 'E-mail is required',
+        EmailValid: v => /.+@.+/.test(v) || 'E-mail must be valid',
+        NameReq: value => !!value || 'Required.',
+      },
+      Login: true,
+    }
+  },
 
-	methods: {
-		Register(){
-			if(this.$refs.form.validate()) {
-				this.Loading = true
-				const AUTH  = firebase.firebase.auth();
-				AUTH.createUserWithEmailAndPassword(this.RegEmail, this.RegPass).then( user => {
-					firebase.firebase.firestore().collection("Users").doc(user.user.uid).set({
-							Name: this.RegName,
-						}).then(() => {
-							this.Loading = false
-							this.$router.replace('/')
-						});
-						this.Loading = false
-						this.$router.replace('/')
-					}, 
-				error => { 
-					this.SnackbarMsg = error.message; 
-					this.Loading = false
-					this.Snackbar = true
-				});
-			}
-		},
+  methods: {
+    Register() {
+      if (this.$refs.form.validate()) {
+        this.Loading = true
+        const AUTH = firebase.firebase.auth();
+        AUTH.createUserWithEmailAndPassword(this.RegEmail, this.RegPass).then(user => {
+          firebase.firebase.firestore().collection("Users").doc(user.user.uid).set({
+            Name: this.RegName,
+          }).then(() => {
+            this.Loading = false
+            this.$router.replace('/')
+          });
+          this.Loading = false
+          this.$router.replace('/')
+        },
+          error => {
+            this.SnackbarMsg = error.message;
+            this.Loading = false
+            this.Snackbar = true
+          });
+      }
+    },
 
-		LogIn(){
-			if(this.$refs.form.validate()) {
-				this.Loading = true
-				const AUTH  = firebase.firebase.auth();
-				AUTH.signInWithEmailAndPassword(this.LoginEmail, this.LoginPassword).then( user => {
-				this.$router.replace('/')
-				this.$eventBus.$emit('Sux');
-				this.Loading = false
-				},  error => { 
-					this.SnackbarMsg = error.message; 
-					this.Loading = false
-					this.Snackbar = true
-				});
-			}
-		},
-	},
+    LogIn() {
+      if (this.$refs.form.validate()) {
+        this.Loading = true
+        const AUTH = firebase.firebase.auth();
+        AUTH.signInWithEmailAndPassword(this.LoginEmail, this.LoginPassword).then(user => {
+          this.$router.replace('/')
+          this.$eventBus.$emit('Sux');
+          this.Loading = false
+        }, error => {
+          this.SnackbarMsg = error.message;
+          this.Loading = false
+          this.Snackbar = true
+        });
+      }
+    },
+  },
 
-	created(){
-		if(this.$store.getters.getUser){
-			this.$router.replace('/')
-    	}
-	}
+  created() {
+    if (this.$store.getters.getUser) {
+      this.$router.replace('/')
+    }
+  }
 }
 </script>
 <style>
 .Title {
-	text-shadow: 0.07em 0.07em 0 rgba(0, 0, 0, 0.1);
+  text-shadow: 0.07em 0.07em 0 rgba(0, 0, 0, 0.1);
 }
 </style>
--- src/views/MyIdeas.vue
+++ src/views/MyIdeas.vue
@@ -17,69 +17,89 @@
           <v-icon>calendar_today</v-icon>
         </v-btn>
       </template>
-      <span>Sort by Date</span>
+      <span>Sort by Completed</span>
     </v-tooltip>
 
-    <v-layout
-      v-masonry
-      transition-duration="1s"
-      item-selector=".item"
-      class="masonry-container"
-      justify-center
-    >
-      <v-flex
-        align="center"
-        justify="center"
-        class="item ma-2 align-center justify-center"
-        v-masonry-tile
-        v-for="Idea in Ideas"
-        :key="Idea.DocID"
-        xs12
-        md4
-      >
-        <v-card hover width="320" class="mx-auto">
-          <v-card-Title class="title">
-            {{ Idea.Title}}
-            <v-spacer></v-spacer>
-            <v-tooltip right>
-              <template v-slot:activator="{ on }">
-                <v-icon v-on="on" v-if="Idea.Completed" class="green--text" small>done_all</v-icon>
-                <v-icon v-on="on" v-else class="orange--text text--lighten-2" small>update</v-icon>
-              </template>
-              <span>{{Idea.Completed ? "Completed" : "Pending"}}</span>
-            </v-tooltip>
-          </v-card-Title>
-          <v-card-text>{{ Idea.Description }}</v-card-text>
-
-          <v-card-actions class="caption grey--text text--darken-3">
-            Added on: {{ Idea.Added }}
-            <br />
-            Visibility: {{ Idea.Visibility }}
-            <v-spacer></v-spacer>
-            <v-menu nudge-top="6" top offset-x>
-              <template v-slot:activator="{ on }">
-                <v-btn icon v-on="on">
-                  <v-icon>more_vert</v-icon>
-                </v-btn>
-              </template>
-              <v-list>
-                <v-list-item v-for="(Option, i) in Options" :key="i">
-                  <v-list-item-title>{{ Option.Title }}</v-list-item-title>
-                </v-list-item>
-              </v-list>
-            </v-menu>
-          </v-card-actions>
-        </v-card>
+    <v-row class="justify-center">
+      <v-flex v-for="x in 3" xs3 :key="x">
+        <vue-content-loading :width="300" :height="130">
+          <rect x="70" y="15" rx="4" ry="4" width="117" height="6.4" />
+          <rect x="70" y="35" rx="3" ry="3" width="85" height="6.4" />
+          <rect x="0" y="80" rx="3" ry="3" width="350" height="6.4" />
+          <rect x="0" y="100" rx="3" ry="3" width="380" height="6.4" />
+          <rect x="0" y="120" rx="3" ry="3" width="201" height="6.4" />
+          <circle cx="30" cy="30" r="30" />
+        </vue-content-loading>
       </v-flex>
-    </v-layout>
+    </v-row>
+
+    <v-row class="justify-center">
+      <v-layout
+        v-masonry
+        transition-duration="1s"
+        item-selector=".item"
+        class="masonry-container"
+        justify-center
+      >
+        <v-flex
+          align="center"
+          justify="center"
+          class="item ma-2 align-center justify-center"
+          v-masonry-tile
+          v-for="Idea in Ideas"
+          :key="Idea.DocID"
+          xs12
+          md4
+        >
+          <v-card hover width="320" class="mx-auto">
+            <v-card-Title class="title">
+              {{ Idea.Title}}
+              <v-spacer></v-spacer>
+              <v-tooltip right>
+                <template v-slot:activator="{ on }">
+                  <v-icon v-on="on" v-if="Idea.Completed" class="green--text" small>done_all</v-icon>
+                  <v-icon v-on="on" v-else class="orange--text text--lighten-2" small>update</v-icon>
+                </template>
+                <span>{{Idea.Completed ? "Completed" : "Pending"}}</span>
+              </v-tooltip>
+            </v-card-Title>
+            <v-card-text>{{ Idea.Description }}</v-card-text>
+
+            <v-card-actions class="caption grey--text text--darken-3">
+              Added on: {{ Idea.Added }}
+              <br />
+              Visibility: {{ Idea.Visibility }}
+              <v-spacer></v-spacer>
+              <v-menu nudge-top="6" top offset-x>
+                <template v-slot:activator="{ on }">
+                  <v-btn icon v-on="on">
+                    <v-icon>more_vert</v-icon>
+                  </v-btn>
+                </template>
+                <v-list>
+                  <v-list-item v-for="(Option, i) in Options" :key="i">
+                    <v-list-item-title>{{ Option.Title }}</v-list-item-title>
+                  </v-list-item>
+                </v-list>
+              </v-menu>
+            </v-card-actions>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-row>
   </v-container>
 </template>
-
+IDEARE
 <script>
 import firebase from "../firebase";
 import store from "../store";
+import VueContentLoading from 'vue-content-loading';
 
 export default {
+  components: {
+    VueContentLoading,
+  },
+
   data() {
     return {
       Ideas: [],
