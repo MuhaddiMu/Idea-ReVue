c5ac9441755861197a4523aa5b51a10ce8adc7ca
MuhaddiMu <Muhaddisshah@gmail.com>
2019-09-23 22:53:27 +0500

    Sort By Completed Ideas Functioanlity Added. To Leanr: sort()

--- src/main.js
+++ src/main.js
@@ -1,27 +1,26 @@
-import Vue from "vue"
-import vuetify from "./plugins/vuetify"
-import App from "./App.vue"
-import router from "./router"
-import store from "./store"
-import vueHeadful from "vue-headful"
-import firebase from "./firebase"
-import Gravatar from "vue-gravatar"
-import { VueMasonryPlugin } from 'vue-masonry';
+import Vue from 'vue'
+import vuetify from './plugins/vuetify'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import vueHeadful from 'vue-headful'
+import firebase from './firebase'
+import Gravatar from 'vue-gravatar'
+import { VueMasonryPlugin } from 'vue-masonry'
 
 Vue.use(VueMasonryPlugin)
 
-
-Vue.component("v-gravatar", Gravatar)
+Vue.component('v-gravatar', Gravatar)
 
 Vue.prototype.$eventBus = new Vue()
 
-Vue.component("vue-headful", vueHeadful)
+Vue.component('vue-headful', vueHeadful)
 
 router.beforeEach((to, from, next) => {
   const currentUser = firebase.firebase.auth().currentUser
   const requiresAuth = to.matched.some(record => record.meta.requiresAuth)
   if (requiresAuth && !currentUser) {
-    next("/Auth")
+    next('/Auth')
   } else if (requiresAuth && currentUser) {
     next()
   } else {
@@ -36,5 +35,5 @@ firebase.firebase.auth().onAuthStateChanged(function (user) {
     store,
     vuetify,
     render: h => h(App)
-  }).$mount("#app")
+  }).$mount('#app')
 })
--- src/views/MyIdeas.vue
+++ src/views/MyIdeas.vue
@@ -1,7 +1,6 @@
 <template>
   <v-container fill-height grid-list-md>
     <vue-headful title="My Ideas | Idea Re-Vue" />
-
     <v-tooltip bottom>
       <template v-slot:activator="{ on }">
         <v-btn @click="SortByComp" small v-on="on" fab depressed>
@@ -23,7 +22,12 @@
     <v-row v-if="!Ideas" class="justify-center">
       <v-flex class="ma-2" v-for="x in 3" xs3 :key="x">
         <v-card hover width="320" class="mx-auto">
-          <vue-content-loading primary="#f3f3f3" secondary="#ecebeb" :width="300" :height="130">
+          <vue-content-loading
+            primary="#f3f3f3"
+            secondary="#ecebeb"
+            :width="300"
+            :height="130"
+          >
             <rect x="6.72" y="8" rx="4" ry="4" width="243.73" height="13.02" />
             <rect x="361" y="95" rx="3" ry="3" width="141.95" height="10.02" />
             <rect x="10" y="36" rx="3" ry="3" width="270" height="6" />
@@ -59,14 +63,26 @@
       >
         <v-card hover width="320" class="mx-auto">
           <v-card-Title class="title">
-            {{ Idea.Title}}
+            {{ Idea.Title }}
             <v-spacer></v-spacer>
             <v-tooltip right>
               <template v-slot:activator="{ on }">
-                <v-icon v-on="on" v-if="Idea.Completed" class="green--text" small>done_all</v-icon>
-                <v-icon v-on="on" v-else class="orange--text text--lighten-2" small>update</v-icon>
+                <v-icon
+                  v-on="on"
+                  v-if="Idea.Completed"
+                  class="green--text"
+                  small
+                  >done_all</v-icon
+                >
+                <v-icon
+                  v-on="on"
+                  v-else
+                  class="orange--text text--lighten-2"
+                  small
+                  >update</v-icon
+                >
               </template>
-              <span>{{Idea.Completed ? "Completed" : "Pending"}}</span>
+              <span>{{ Idea.Completed ? "Completed" : "Pending" }}</span>
             </v-tooltip>
           </v-card-Title>
           <v-card-text>{{ Idea.Description }}</v-card-text>
@@ -94,15 +110,14 @@
     </v-layout>
   </v-container>
 </template>
-IDEARE
 <script>
-import firebase from "../firebase";
-import store from "../store";
-import VueContentLoading from 'vue-content-loading';
+import firebase from "../firebase"
+import store from "../store"
+import VueContentLoading from "vue-content-loading"
 
 export default {
   components: {
-    VueContentLoading,
+    VueContentLoading
   },
 
   data() {
@@ -114,37 +129,46 @@ export default {
         { Title: "Mark as Completed" },
         { Title: "Delete" }
       ]
-    };
+    }
   },
   methods: {
     GetIdeas() {
-      let self = this;
+      let self = this
       firebase.firebase
         .firestore()
         .collection("Ideas")
         .where("AddedBy", "==", this.$store.getters.getUser.uid)
         .get()
-        .then(function (querySnapshot) {
-
+        .then(function(querySnapshot) {
           self.Ideas = []
 
-
-          querySnapshot.forEach(function (doc) {
+          querySnapshot.forEach(function(doc) {
             var DocData = doc.data()
             DocData.DocID = doc.id
             self.Ideas.push(DocData)
-          });
+          })
         })
-        .catch(function (error) {
+        .catch(function(error) {
           console.log("Error getting documents: ", error)
-        });
+        })
     },
-    SortByComp() { },
-    SortByDate() { },
+    SortByComp() {
+      let UpdatedIdeas = this.Ideas.sort((a, b) => {
+        if (a.Completed > b.Completed) {
+          return -1
+        }
+      })
 
+      this.Ideas = {}
+
+      setTimeout(() => {
+        this.Ideas = UpdatedIdeas
+      }, 1)
+    },
+    SortByDate() {}
   },
   created() {
-    this.GetIdeas();
+    this.GetIdeas()
   }
 }
 </script>
