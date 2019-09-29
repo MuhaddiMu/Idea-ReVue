809a6b1e2c4139f048789e0d45d111065033bfd5
MuhaddiMu <Muhaddisshah@gmail.com>
2019-09-30 00:30:48 +0500

    Idea Deleted Functioanlity Added

---
+++ src/views/DeleteIdea.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-row justify="center">
+    <v-dialog v-model="Dialog" max-width="290">
+      <v-card>
+        <v-card-title class="headline">Delete Idea?</v-card-title>
+        <v-card-text>You can edit the Idea if you just need to change something. Why not have a list of your wonderful thoughts!</v-card-text>
+        <v-card-actions>
+          <div class="flex-grow-1"></div>
+
+          <v-btn text @click="Dialog = false">Cancel</v-btn>
+
+          <v-btn color="error" text @click="ConfirmDelete(IdeaID)">Delete</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </v-row>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      Dialog: false,
+      IdeaID: '',
+    }
+  },
+
+  methods: {
+    ShowModal(IdeaID) {
+      this.Dialog = true
+      this.IdeaID = IdeaID
+    },
+
+    ConfirmDelete(IdeaID) {
+       this.$emit('ConfirmDelete', IdeaID)
+       this.Dialog = false
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
